d85f3ddee52b361281824b61ece88ba91149a7fd
fujimoto <fujimoto04@gmail.com>
2022-10-15 17:17:58 +0900

    2022-10-15

--- shyobi/js/master.js
+++ shyobi/js/master.js
@@ -121,7 +121,7 @@ jQuery(function($) {
 			slidesToShow :  3,
 			slidesToScroll :  1,
 			autoplay :  true,
-			autoplaySpeed :  2000,
+			autoplaySpeed :  3000,
 			dots: true,
 			responsive: [
 				{
